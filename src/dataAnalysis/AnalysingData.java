ab30599a81065d068c030f1eb510cdfd655006bc
Ben Philps <bp413@cam.ac.uk>
2018-09-01 18:04:48 +0100

    Created the function test read speed, and the class ByteBufferInput, to setup a methof for reading the file using a buffer and to test that it can read in the whole file (which it seems to do quite quickly on the first test).

--- src/dataAnalysis/AnalysingData.java
+++ src/dataAnalysis/AnalysingData.java
@@ -1,5 +1,7 @@
 package dataAnalysis;
 
+import java.io.IOException;
+
 public class AnalysingData {
 
     //This class is for messing about for reading in the data and looking at it etc.
@@ -17,14 +19,25 @@ public class AnalysingData {
         System.out.println("analysis");
     }
 
-    public void run(){
+    public void run() throws IOException{
 
+        testReadSpeed();
     }
 
 
-    public void testReadSpeed(){
+    public void testReadSpeed() throws IOException {
         // Tests read speeds of different reader inputs,
         // to just read the whole file and perform a comparison on each digit
+
+        int bufferSize = 5000;
+        long time = System.currentTimeMillis();
+
+        System.out.println("Using a Byte buffer reader");
+        ByteBufferInput byteBufferInput = new ByteBufferInput();
+        byteBufferInput.run(f1, bufferSize);
+        System.out.print("time: ");
+        System.out.println(System.currentTimeMillis() - time);
+        System.out.println("-------------\n\n");
     }
 
 }
---
+++ src/dataAnalysis/ByteBufferInput.java
@@ -0,0 +1,43 @@
+package dataAnalysis;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+
+public class ByteBufferInput {
+
+    public void run(String f1, int bufferSize) throws IOException {
+
+        System.out.println("USING BYTE BUFFER AND INPUT STREAM");
+
+        RandomAccessFile f = new RandomAccessFile(f1,"rw");
+        //RandomAccessFile f = new RandomAccessFile("/Users/Ben/Cambridge/IB/FirstTerm/Further Java/my code/Examples/testfile2","rw");
+        DataInputStream d = new DataInputStream(new BufferedInputStream(new FileInputStream(f.getFD())));
+        // This code above is similar to the output stuff, except this time it reads.
+
+        int intSize = 4;
+        byte[] data = new byte[intSize * bufferSize];
+
+        int bytesRead = d.read(data);
+        //bytes read returns the number of bytes read.
+        //I will play around with buffer size.
+
+        while(bytesRead != -1) {
+            doSomethingWithData(data, bytesRead);
+
+            bytesRead = d.read(data);
+        }
+        d.close();
+
+    }
+
+    private void doSomethingWithData(byte[] data, int bytesRead){
+        ByteBuffer bb = ByteBuffer.wrap(data);
+        for(int i = 0; i < bytesRead/4; i++){
+            //System.out.println("value read in is: " + bb.getInt());
+            if(bb.getInt() < 0) {
+                System.out.println("ooh a negative"); //there are no negatives, im just giving it a pointless comparison to do.
+            }
+        }
+    }
+
+}
