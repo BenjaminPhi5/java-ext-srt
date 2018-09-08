deec0da9af673fe88f567471f19e24f367c1d69e
Ben Philps <bp413@cam.ac.uk>
2018-09-08 17:25:44 +0100

    it seems trying to read in the values and then processing them on a different thread takes more time! Not sure I believe this. hmm.

---
+++ src/dataAnalysis/ReadIn/DirectBufferThreaded.java
@@ -0,0 +1,10 @@
+package dataAnalysis.ReadIn;
+
+import com.sun.org.apache.regexp.internal.RE;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class DirectBufferThreaded {
+
+}
